a606ebb7c818e9da89e60508b480ee231d8cb591
Andy Chen <andyml01hello@gmail.com>
2018-11-21 23:52:26 +0800

    Add project files.

---
+++ IoT-Simulator/MessageModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IoT_Simulator
+{
+    public class MessageModel
+    {
+        public long eventTime { get; set; } //Epoch Unhix TimeStamp(milseconds
+        public string uid { get; set; }
+        public string systemtype { get; set; }
+        public string devtype { get; set; }
+        public string devname { get; set; }
+        public string unit { get; set; }
+        public string describe { get; set; }
+        public string value { get; set; }
+
+    }
+
+    public class CallMethodModel
+    {
+        public string DeviceId { get; set; }
+        public string Status { get; set; }
+        public int OperationMinutes { get; set; }
+    }
+}
